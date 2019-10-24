2b68d047cc341fb3a2c314960ba8fcf38477aecf
MACKAY Jill <jmackay4@ed.ac.uk>
2019-10-24 14:25:38 +0100

    app working but data input failing

--- app/server.R
+++ app/server.R
@@ -1,7 +1,67 @@
 library(shiny)
+library(tidyverse)
+library(readr)
+library(tidytext)
+library(zoo)
+library(textstem)
+library(wesanderson)
+library(wordcloud)
+library(patchwork)
 
 shinyServer(function(input, output, session){
   
+  # --------- Functions for the server ------------
+  
+  whats_rd <- function (x, sender1, sender2) {
+    my_stops <- tibble(word = c("omit", "medium", "https"),
+                       lexicon = c("my_stops", "my_stops", "my_stops"))
+    my_stops<-rbind(stop_words, my_stops)
+    
+    sender1 <- enquo(sender1)
+    sender2 <- enquo(sender2)
+    
+    read_lines(x) %>% 
+      tibble() %>% 
+      rename(msg_text = ".") %>% 
+      mutate (msg_text = parse_character(msg_text)) %>% 
+      mutate (msg_stamp = str_extract(msg_text, "\\d\\d\\/\\d\\d\\/\\d\\d\\d\\d\\,\\ \\d\\d\\:\\d\\d\\ \\-\\ [^0-9]+\\: ")) %>% 
+      separate(msg_stamp, into = c("date", "sender"), sep = " - ") %>% 
+      mutate (msg_text = str_remove(msg_text, "\\d\\d\\/\\d\\d\\/\\d\\d\\d\\d\\,\\ \\d\\d\\:\\d\\d\\ \\-\\ [^0-9]+\\: ")) %>% 
+      mutate (sender = na.locf(sender, na.rm = FALSE)) %>% 
+      mutate (date = na.locf(date, na.rm = FALSE)) %>% 
+      filter(!is.na(msg_text),
+             !is.na(date)) %>% 
+      separate(date, into = c("date", "time"), sep =",") %>% 
+      mutate (date = lubridate::dmy(date)) %>% 
+      mutate (shortsend = case_when (str_detect(sender, !!sender1) ~ !!sender1,
+                                     str_detect(sender, !!sender2) ~ !!sender2)) %>% 
+      unnest_tokens(word, msg_text) %>% 
+      mutate (word = (lemmatize_strings(word))) %>% 
+      anti_join(my_stops) %>% 
+      filter(str_detect(word, '[^0-9]'))
+    
+  }
+  
+  # ---------- Importing Data -----------------
+  
+  dat <- reactive ({
+    
+    infile <- input$file
+    name1 <- input$name1
+    name2 <- input$name2
+    
+    whats_rd(file, name1, name2)
+    
+    
+  })
+  
+  # ---------- Output raw data --------------
+  
+  output$raw_data <- renderDT({
+    if (!is.null(dat())) {
+      dat()
+    }
+  })
   
   
 })
--- app/ui.R
+++ app/ui.R
@@ -1,11 +1,21 @@
 library(shiny)
+library(DT)
 
 shinyUI(
   navbarPage(title = "Visualiser",
              id = "navbar",
              
              tabPanel(title = "Import data",
-                      mainPanel(title = "Import more data")
+                      mainPanel(
+                        fileInput(inputId = "file",
+                                  label = "Upload your text file here"),
+                        textInput(inputId = "name1",
+                                  label = "Type the name of person 1 "),
+                        textInput(inputId = "name2",
+                                  label = "Type the name of person 2"),
+                        
+                        DTOutput("raw_data")
+                        )
                       ),
              
              tabPanel(title = "Another tab",

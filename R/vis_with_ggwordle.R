3a88658dcd9152091a67807162de87b1a346bccb
MACKAY Jill <jmackay4@ed.ac.uk>
2019-11-12 13:15:46 +0000

    ggwordcloud working

--- R/vis_with_ggwordle.R
+++ R/vis_with_ggwordle.R
@@ -16,14 +16,17 @@ library(wesanderson)
 dat %>% 
   filter(str_detect(word, '[^0-9]')) %>% 
   count (word, shortsend, sort = TRUE) %>%
-  top_n(5)  %>% 
-  ggplot(aes(label = word)) +
-  geom_text_wordcloud() +
-  theme_minimal()
-  
+  top_n(100)  %>% 
+  ggplot(aes(label = word, size = n, color = shortsend)) +
+  geom_text_wordcloud_area() +
+  scale_size_area(max_size = 20) +
+  theme_minimal() +
+  scale_color_manual(values = wes_palette("Darjeeling2"))
 
+# this works
 
 tibble(word = c("A", "B", "C", "D"),
        size = c(4,3,2,1)) %>% 
-  ggplot(aes(x = word, y = size)) +
-  geom_bar(stat = "identity")
+  ggplot(aes(label = word, size = size)) +
+  geom_text_wordcloud_area() +
+  theme_minimal()
